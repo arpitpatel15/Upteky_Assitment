069d9204149d0ad8f59094014dbb8fa00334abb4
Arpit Patel <codewithme442@gmail.com>
2025-11-15 13:48:32 +0530

    deployed url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/feedback";
+const API_BASE_URL = "https://upteky-assitment-1.onrender.com/api/feedback";
 
 export const feedbackAPI = {
   submitFeedback: async (data) => {
